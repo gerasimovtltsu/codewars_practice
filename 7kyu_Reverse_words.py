80ba244d76f8dbeed64153bda10b022e7e10c114
old_qqw <theavg@yandex.ru>
2023-03-29 21:23:30 +0400

    feat: complete reverse words task

---
+++ 7kyu_Reverse_words.py
@@ -0,0 +1,13 @@
+"""
+# https://www.codewars.com/kata/5259b20d6021e9e14c0010d4/train/python
+
+Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+Examples
+"This is an example!"	==>	"sihT si na !elpmaxe"
+"double  spaces"			==>	"elbuod  secaps"
+"""
+
+
+def reverse_words(text):
+	return " ".join([word[::-1] for word in text.split(" ")])
