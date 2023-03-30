69d64dcf73b586aab4af22a1c643fabaf6cfa174
old_qqw <theavg@yandex.ru>
2023-03-30 18:01:00 +0400

    feat: complete Square(n) Sum task

---
+++ 8kyu/Square(n)_Sum.py
@@ -0,0 +1,13 @@
+'''
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 
+1^2 + 2^2 + 2^2 = 9
+'''
+
+def square_sum(numbers, result_sum = 0):
+	for i in numbers:
+		result_sum += i ** 2
+	return result_sum
+
+print(square_sum([-1, -2]))
