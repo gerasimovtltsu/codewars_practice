b57fe15901419366026fdfb239815c3828c84129
Anton Gerasimov <82588219+oldqqw@users.noreply.github.com>
2023-04-01 11:55:30 +0400

    feat: comlete tasks

---
+++ 7kyu/Binary_Addition.py
@@ -0,0 +1,5 @@
+def add_binary(a, b):
+    return bin(a + b)[2:]
+
+
+print(add_binary(1, 1))
---
+++ 8kyu/Find_the_smallest_integer_in_the_array.py
@@ -0,0 +1,5 @@
+def find_smallest_int(arr):
+    return min(arr)
+
+
+print(find_smallest_int([2, 1, -4, 6, 9]))
